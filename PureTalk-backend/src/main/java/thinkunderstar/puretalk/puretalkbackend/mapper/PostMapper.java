7cf8245ddbc858d23772d5f8ee9769d9253b25eb
jiang <185388966@qq.com>
2026-04-28 09:24:25 +0800

    2026.04.28/09:24

--- PureTalk-backend/src/main/java/thinkunderstar/puretalk/puretalkbackend/mapper/PostMapper.java
+++ PureTalk-backend/src/main/java/thinkunderstar/puretalk/puretalkbackend/mapper/PostMapper.java
@@ -1,7 +1,16 @@
 package thinkunderstar.puretalk.puretalkbackend.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import thinkunderstar.puretalk.puretalkbackend.entity.Post;
 
+import java.util.List;
+
 public interface PostMapper extends BaseMapper<Post> {
+    @Select("SELECT *, MATCH(title) AGAINST(#{keyword} IN NATURAL LANGUAGE MODE) AS relevance " +
+            "FROM posts " +
+            "WHERE MATCH(title) AGAINST(#{keyword} IN NATURAL LANGUAGE MODE) " +
+            "ORDER BY relevance DESC")
+    List<Post> searchByTitle(@Param("keyword") String keyword);
 }
